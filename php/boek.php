956422d436436e7fcccf46ae6a9fd08a52e7507b
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-05-18 13:06:24 +0200

    ww

--- php/boek.php
+++ php/boek.php
@@ -2,23 +2,23 @@
 <?php include "include/Navbar.php"; ?>
 <?php include "include/Connectdb.php"; ?>
 <?php
-    $sql = "SELECT * FROM `boeken` ORDER BY `boeken`.`Rank` ASC"; 
+    $id = $_POST['id'];
+    $sql = "SELECT * FROM `boeken` WHERE id = :id"; 
     $sth = $db->prepare($sql); 
-    $sth->execute(); 
+    $sth->execute(['id' => $id]); 
     while($row = $sth->fetch()) { 
-    ?>
-    <img src="/media/boeken/<?php echo $row["img"]?>.png">
-    <?php
+    echo $row["id"];
     echo "<br>";
     echo $row["Title"];
     echo "<br>";
+    echo $row["Description"];
+    echo "<br>";
+    echo $row["rank"];
+    echo "<br>";
     echo $row["Price"];
     echo "<br>";
+    echo "<br>";
     ?>
-    <form method="post" action="/php/boek.php">
-        <input type="hidden" name="id" value="<?php echo $row["id"]?>">
-        <input type="submit" class="btn btn-primary">
-    </form>
-
+    <img src="/media/img/steren/ster<?php echo $row["rank"]?>.png">
     <?php } ?>
 <?php include "include/Footer.php"; ?>
